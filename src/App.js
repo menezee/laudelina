0bf74ce7850e0985830fb0e159c7ac338ebcde98
Erich Menezes <emenezes@thoughtworks.com>
2019-05-06 13:54:13 -0700

    :tada:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {AutoComplete, Col, Layout, Row} from 'antd';
+
+const {Header, Footer, Content} = Layout;
+
+const dataSource = [
+  {
+    value: '1',
+    text: 'Quais são meus direitos?',
+    answer: 'Direito 1, Direito 2, e Direito 3',
+  },
+  {
+    value: '2',
+    text: 'Qual é o salário mínimo?',
+    answer: 'R$1.000,00 (mil)',
+  },
+  {
+    value: '3',
+    text: 'Quanto tempo de almoço eu tenho por dia?',
+    answer: '1h',
+  },
+  {
+    value: '4',
+    text: 'Quantos dias de férias tenho direito?',
+    answer: '30 dias por ano',
+  },
+];
+
+function filterOptionHandler(inputValue, option) {
+  return option.props.children.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1;
+}
 
 function App() {
+  const [selectedQuestion, setSelectedQuestion] = useState();
+  const onSelect = e => {
+    setSelectedQuestion(e);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header>
+        <span style={{color: 'white'}}>
+        Laudelina
+        </span>
+      </Header>
+      <Content>
+        <Row type='flex' justify='center'>
+          <Col span={18}>
+            <AutoComplete
+              dataSource={dataSource}
+              style={{width: '100%'}}
+              onSelect={onSelect}
+              filterOption={filterOptionHandler}
+              placeholder='Pergunta'
+            />
+          </Col>
+        </Row>
+        <Row type='flex' justify='center' style={{ paddingTop: '20px' }}>
+          <Col span={18}>
+            {
+              dataSource
+                .filter(({value}) => value === selectedQuestion)
+                .map(({answer}, i) => (
+                  <span key={i}>
+                    {answer}
+                  </span>
+                ))
+            }
+          </Col>
+        </Row>
+      </Content>
+      <Footer>
+        Footer
+      </Footer>
+    </>
   );
 }
 
