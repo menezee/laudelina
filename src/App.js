a0ccaa743b2ad71a372f8b6a19081b05437e82df
Erich Menezes <erich_menezes@gap.com>
2019-07-26 19:17:31 -0700

    :construction:

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import {Col, Collapse, Icon, Layout, Row, Typography} from 'antd';
+import {Input} from 'antd';
 import Panel from './components/panel';
 import Header from './components/header/';
 import questions from './data/questions';
 import styles from './app.module.scss';
 
-const {Content} = Layout;
+const {Search} = Input;
+  const {Content} = Layout;
 const {Title} = Typography;
 
 function App() {
+  const [filterText, setFilterText] = useState('');
+
   return (
     <Fragment>
       <Header/>
       <Content>
         <Row type='flex' justify='center'>
           <Col xs={22} sm={20} md={20} lg={12}>
+            <Search placeholder="input search text" onSearch={value => setFilterText(value)} enterButton/>
             {
               questions
+                .filter(({questions}) => (
+                  questions.some(({question, answer}) =>
+                    question.includes(filterText) || answer.includes(filterText))
+                ))
                 .map(({questions, category}, i) => (
                   <div key={`container-${i}`}>
                     <Title key={`title-${i}`} level={3} className={styles.category}>{category}</Title>
